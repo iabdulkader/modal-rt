78f8b0ac7dc1d7107a8d2ec6591a9c87e24161d0
iabdulkader <abdulkadernow@gmail.com>
2022-12-28 01:46:10 +0600

    TEsting Sometings

--- src/components/ModalWrapper/ModalWrapper.tsx
+++ src/components/ModalWrapper/ModalWrapper.tsx
@@ -17,45 +17,49 @@ const ModalWrapper = React.memo(({ id, children, animation = true, customTrigger
 
     const zoom = keyframes`
         0% {
-            transform: scale(0);
-            transform: translate(0, 0);
+            transform: scale(0);     
         }
         50% {
-            transform: scale(0.7);
-            transform: translate(-55%, -55%);
+            transform: scale(0.7);   
         }
         100% {
-            transform: scale(1);
-            transform: translate(-50%, -50%);
+            transform: scale(1);         
         }
     `;
 
-    const Parent = styled('div')`
-        position: relative;
-        min-width: 100vw;
-        height: 100%;
-    `
-
-    const OverLay = styled('div')<OverLayProps>`
+    const Parent = styled('div')<OverLayProps>`
         position: fixed;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: rgba(0, 0, 0, 0.6);
         ${({ id }) => `z-index: ${999+Number(id)}`};
+    `
+
+    const OverLay = styled('div')`
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.6); 
     `;
 
-    const Wrapper = styled('div')<WrapperProps>`
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%); 
-        padding: 1rem;
+    const Container = styled('div')`
+        width: 100%;
+        height: 100%;
         display: flex;
-        flex-direction: column;
+        justify-content: center;
+        align-items: center;    
+`
+
+    const Wrapper = styled('div')<WrapperProps>`
+        padding: 1rem;
+        position: relative;
+        display: flex;
+        justify-content: center;
+        max-width: 100%;
         max-height: 100%;
-        ${({ id }) => `z-index: ${1000+Number(id)}`};
         scale: 1;
         transition: all 300ms ease-in-out;
         ${({ animation }) => animation && `animation: ${zoom} 250ms linear`};
@@ -101,31 +105,39 @@ const ModalWrapper = React.memo(({ id, children, animation = true, customTrigger
 
     
     return (
-    <Parent>
-        <OverLay id={id} onClick={close}></OverLay>
+    <Parent id={id}>
 
+      <OverLay id={id} onClick={close} />
+
+      <Container>
         <Wrapper id={id} animation={animation}>
+        
 
-        {customTrigger === false ? 
-            (
-                <ButtonDiv>
-                    <ButtonContainer>
-                        <Button onClick={close}>
-                            <svg viewBox="0 0 512 512">
-                                <path
-                                    fill="#fff"
-                                    d="M443.6 387.1 312.4 255.4l131.5-130c5.4-5.4 5.4-14.2 0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L256 197.8 124.9 68.3c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L68 105.9c-5.4 5.4-5.4 14.2 0 19.6l131.5 130L68.4 387.1c-2.6 2.6-4.1 6.1-4.1 9.8 0 3.7 1.4 7.2 4.1 9.8l37.4 37.6c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1L256 313.1l130.7 131.1c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1l37.4-37.6c2.6-2.6 4.1-6.1 4.1-9.8-.1-3.6-1.6-7.1-4.2-9.7z"
-                                />
-                            </svg>
-                        </Button>
-                    </ButtonContainer>
-                </ButtonDiv>
-            ) : null
-        }
+            {customTrigger === false ? 
+                (
+                    <ButtonDiv>
+                        <ButtonContainer>
+                            <Button onClick={close}>
+                                <svg viewBox="0 0 512 512">
+                                    <path
+                                        fill="#fff"
+                                        d="M443.6 387.1 312.4 255.4l131.5-130c5.4-5.4 5.4-14.2 0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L256 197.8 124.9 68.3c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L68 105.9c-5.4 5.4-5.4 14.2 0 19.6l131.5 130L68.4 387.1c-2.6 2.6-4.1 6.1-4.1 9.8 0 3.7 1.4 7.2 4.1 9.8l37.4 37.6c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1L256 313.1l130.7 131.1c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1l37.4-37.6c2.6-2.6 4.1-6.1 4.1-9.8-.1-3.6-1.6-7.1-4.2-9.7z"
+                                    />
+                                </svg>
+                            </Button>
+                        </ButtonContainer>
+                    </ButtonDiv>
+                ) : null
+            }
 
-            {children}
+            
+          {children}
 
         </Wrapper>
+
+    </Container>
+
+        
     </Parent>
     )
 });
